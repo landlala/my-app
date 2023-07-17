579848e47226a045192d5b090aae2eaca3850aad
landlala <land_lala@naver.com>
2023-07-17 22:55:53 +0900

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
 import Tv from "./Routes/Tv";
@@ -6,7 +6,7 @@ import Header from "./Components/Header";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header></Header>
       <Routes>
         <Route path = "/" element = {<Home />}>
@@ -15,7 +15,7 @@ function App() {
         <Route path = "/search" element = {<Search />}></Route>
         <Route path = "tv" element = {<Tv />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
